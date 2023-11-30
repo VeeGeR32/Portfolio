3a1e433cfd4b17ba3d14764e6b0eca5693689449
VeeGeR32 <dialloalioune32@icloud.com>
2023-11-30 09:58:43 +0100

    hero text responsive

--- src/Components/HeroText.jsx
+++ src/Components/HeroText.jsx
@@ -13,7 +13,7 @@ const HeroText = () => {
           initial={{ opacity: 0 }}
           whileInView={{ opacity: 1 }}
           transition={{ duration: 0.6, type: "spring" }}
-          className="text-center flex justify-center items-center flex-col opacity-100 text-7xl sm:text-9xl cursor-default"
+          className="text-center flex justify-center items-center flex-col opacity-100 text-6xl sm:text-9xl cursor-default"
           style={{ fontFamily: "SuperMario" }}
         >
           <m.span
